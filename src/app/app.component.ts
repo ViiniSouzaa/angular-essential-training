972847f5fd231485d2f4d8b218417206d7d8f983
Vinicius <viniciussilvasouza62@gmail.com>
2021-11-09 16:41:51 -0300

    Styling a component - part1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'mw-app',
   templateUrl: './app.component.html',
-  styles: [`
-    h1 {  color: #ffffff;}
-    `, `
-    .description{
-            font-style: italic;
-            color: green;
-       }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent{
 
