1f7e2c164beed74746d905da0447ac8da600226c
Vinicius <viniciussilvasouza62@gmail.com>
2021-11-09 15:51:57 -0300

    The component template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'mw-app',
-  template: '<h1>My App</h1>'
+  templateUrl: './app.component.html'
 })
 export class AppComponent{
 
