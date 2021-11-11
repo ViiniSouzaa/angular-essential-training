289e7659d1df15dcdb7dac1639632f0cff1a8cab
Vinicius <viniciussilvasouza62@gmail.com>
2021-11-11 18:08:52 -0300

    Working with events in directives

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -1,11 +1,18 @@
-import { Directive, HostBinding, Input } from "@angular/core";
+import { Directive, HostBinding, HostListener, Input } from "@angular/core";
 
 @Directive({
   selector: '[mwFavorite]'
 })
 
 export class FavorireDirective {
-    @HostBinding('class.is-favorite') isFavorite;
+    @HostBinding('class.is-favorite') isFavorite = true;
+    @HostBinding('class.is-favorite-hovering') hovering = false;
+    @HostListener('mouseenter') onMouseEnter(){
+      this.hovering = true;
+    }
+    @HostListener('mouseleave') onMouseLeave(){
+      this.hovering = false;
+    }
     @Input() set mwFavorite(value){
       this.isFavorite = value;
     }
