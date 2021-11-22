67678fbb6972cb97f5d8bed9cb497e3552f766d0
Vinicius <viniciussilvasouza62@gmail.com>
2021-11-22 11:03:12 -0300

    Routing - part 7

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,11 @@
 import { RouterModule, Routes } from "@angular/router";
 import { MediaItemListComponent } from "./media-item/list/media-item-list.component";
 
-
 const appRoutes: Routes = [
+  {
+  path: 'add',
+  loadChildren: () => import('./media-item/form/new-item/new-item.module').then(m => m.NewItemModule)
+},
   { path: ':medium', component: MediaItemListComponent },
   { path: '', redirectTo: 'all', pathMatch: 'full' }
 
